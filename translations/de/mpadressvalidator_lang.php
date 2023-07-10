5cdf0d5147eb28b96fe3f9845dfe2330bf1ceeae
marvinpoehls <poehls.marvin@gmail.com>
2023-07-10 12:06:31 +0200

    fixed language files for admin

--- translations/de/mpadressvalidator_lang.php
+++ translations/de/mpadressvalidator_lang.php
@@ -4,9 +4,5 @@ $sLangName = 'Deutsch';
 
 $aLang = [
     'charset' => 'UFT-8',
-    'ADDRESSFILE' => 'Adress Datei:',
-    'ADDRESSVALIDATION_FILE_ERROR' => 'Es wurde keine CSV Datei hochgeladen.',
-    'ADDRESSVALIDATION_HEADERS_ERROR' => 'Die CSV Header sind falsch.',
-    'ADDRESSVALIDATION_UPLOAD_COMPLETE' => 'Die Datei wurde erfolgreich hochgeladen.',
     'ADDRESSVALIDATION_VALIDATION_ERROR' => 'Die Postleitzahl, der Ort und das Land stimmen nicht über ein.',
 ];
--- translations/en/mpadressvalidator_lang.php
+++ translations/en/mpadressvalidator_lang.php
@@ -4,9 +4,5 @@ $sLangName = 'English';
 
 $aLang = [
     'charset' => 'UFT-8',
-    'ADDRESSFILE' => 'Address Datei:',
-    'ADDRESSVALIDATION_FILE_ERROR' => 'There was no csv file uploaded.',
-    'ADDRESSVALIDATION_HEADERS_ERROR' => 'The csv headers are wrong.',
-    'ADDRESSVALIDATION_UPLOAD_COMPLETE' => 'The file upload was completed.',
     'ADDRESSVALIDATION_VALIDATION_ERROR' => 'The zip, city and country doesnt match.',
 ];
